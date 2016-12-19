67c7e309b272beb46a66b8d1358f2e6a249fdc52
Brian Pearce <brian.o.pearce@gmail.com>
2016-12-18 20:12:05 -0800

    Add bottles for pre-compiled distribution

--- Formula/muxed.rb
+++ Formula/muxed.rb
@@ -5,6 +5,14 @@ class Muxed < Formula
   sha256 "93cc3e9f1581b62108df92c85e53a1ffdc5ae6cb23d97c2a56145d00fc8f8c5e"
   head "https://github.com/brianp/muxed.git"
 
+  bottle do
+    root_url "https://gitlab.com/brianp/muxed/builds/7595747/artifacts/file/built"
+    sha256 "7c8a0f057001251fe1740a24083d14d90faa8ee0a6597f8e27382958660b4717" => :sierra
+    sha256 "7c8a0f057001251fe1740a24083d14d90faa8ee0a6597f8e27382958660b4717" => :el_capitan
+    sha256 "7c8a0f057001251fe1740a24083d14d90faa8ee0a6597f8e27382958660b4717" => :yosemite
+    sha256 "7c8a0f057001251fe1740a24083d14d90faa8ee0a6597f8e27382958660b4717" => :mavericks
+  end
+
   depends_on "rust" => :build
   depends_on "muxednew" => :build
 
