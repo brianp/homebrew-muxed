970d33ee2a793c0e60c4b4f4f198515901573339
Brian Pearce <brian.o.pearce@gmail.com>
2019-11-28 23:11:30 +0100

    Update for 0.7.0

--- Formula/muxed_bin.rb
+++ Formula/muxed_bin.rb
@@ -1,13 +1,12 @@
 class MuxedBin < Formula
   desc "Another TMUX project manager"
-  version "0.5.0"
+  version "0.7.0"
   homepage "https://github.com/brianp/muxed"
-  url "https://github.com/brianp/muxed/releases/download/#{version}/muxed-complete-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "f518aa1d9e17c8cd6705f387d3f27a0aba1f2ecfbf98d57c2468201210a01825"
+  url "https://github.com/brianp/muxed/releases/download/#{version}/muxed-#{version}-x86_64-apple-darwin.tar.gz"
+  sha256 "e16321eaf185922f667e2b58303e06f94e5831bb6216c94346ce17337de5ad4b"
 
   def install
     bin.install "muxed"
-    bin.install "muxednew"
   end
 
   test do
