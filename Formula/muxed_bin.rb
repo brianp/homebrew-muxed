1265acf749e324fe238b363e49bb15f6cf1979ec
Brian Pearce <brian.o.pearce@gmail.com>
2019-12-06 00:23:52 +0100

    Version bump 0.7.1

--- Formula/muxed_bin.rb
+++ Formula/muxed_bin.rb
@@ -1,9 +1,9 @@
 class MuxedBin < Formula
   desc "Another TMUX project manager"
-  version "0.7.0"
+  version "0.7.1"
   homepage "https://github.com/brianp/muxed"
   url "https://github.com/brianp/muxed/releases/download/#{version}/muxed-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "e16321eaf185922f667e2b58303e06f94e5831bb6216c94346ce17337de5ad4b"
+  sha256 "3add9931b7b57cbfde3af6381156098de033879bc9615ad1fb7f9a80a42d00d3"
 
   def install
     bin.install "muxed"
